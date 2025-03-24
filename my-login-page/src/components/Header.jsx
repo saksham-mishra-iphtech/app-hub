f74320e2346a1f040fd5e1548f63483feff2e0a8
saksham-mishra-iphtech <Saksham.mishra@iphtechnologies.com>
2025-03-24 11:38:26 +0530

    First Page (Dashboard)
    created a dashboard page where all the project details are availabe

--- my-login-page/src/components/Header.jsx
+++ my-login-page/src/components/Header.jsx
@@ -1,23 +1,31 @@
-import React from 'react'
-import { Button } from './Button'
-import { useNavigate } from 'react-router-dom'
+import React from "react";
+import { IoIosArrowBack } from "react-icons/io";
+import { useNavigate } from "react-router-dom";
 
-const Header = ({heading}) => {
-    const navigate=useNavigate();
-    const handleback =()=>{
-        navigate('/dashboard')
+const Header = ({ heading, Todo }) => {
+  const navigate = useNavigate();
 
-        
-    }
-
+  const handleBack = () => {
+    navigate("/dashboard");
+  };
 
   return (
-    <div className='flex gap-100 align-center items-center mx-40 my-10'>
-       <Button text="Back" isPrimary onclick={handleback}/>
-       <div className='font-bold text-4xl' >{heading}</div>
-      
+    <div 
+      className={`fixed top-0 left-0 w-full bg-white z-1 m-0 p-0 flex flex-col sm:flex-row ${Todo ? "px-8" : "px-6 "} sm:px-3  md:px-5 lg:px-40 py-6`}
+      style={{ borderBottom: "none" }}
+    >
+      <div className="self-start">
+        <button className="flex text-center items-center font-semibold text-xl text-blue-900" onClick={handleBack}>
+          <div><IoIosArrowBack /></div>
+          <div>Back</div>
+        </button>
+      </div>
+      <div className="w-full text-center font-bold text-2xl sm:text-3xl md:text-4xl mt-4 sm:mt-2">
+        {heading}
+      </div>
+      <div className="hidden sm:block w-20 sm:w-24 md:w-32 lg:w-40"></div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
